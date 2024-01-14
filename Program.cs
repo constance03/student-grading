9aea252455a8a55c5cc5022dbed96484270bdbe7
constance03 <maria-constance@hotmail.com>
2024-01-14 18:23:49 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿// initialize variables - graded assignments
+using System.Threading.Tasks.Dataflow;
+
+int examAssignments = 5;
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
+int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
+int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
+int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };    
+
+// Student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
+int[] studentScores = new int[10];
+string currentStudentLetterGrade = "";
+decimal studentAverage = 0;
+decimal extraCreditAverage = 0;
+decimal pointsExtraCredit = 0;
+
+// Write the Report Header to the console
+Console.WriteLine("Student\t\tExam Score\t\tOverall Grade\t\tExtra Credit\n");
+
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+    int extraCreditSum = 0;
+
+    if (currentStudent == "Sophia")
+       studentScores = sophiaScores;
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+    else if (currentStudent == "Becky")
+        studentScores = beckyScores;
+    else if (currentStudent == "Chris")
+        studentScores = chrisScores;
+    else if (currentStudent == "Eric")
+        studentScores = ericScores;
+    else if (currentStudent == "Gregor")
+        studentScores = gregorScores;
+    else
+        continue;
+
+    // initialize/reset the sum of scored assignments
+    int sumAssignmentScores = 0;
+
+    // initialize/reset the calculated average of exam + extra credit scores
+    decimal currentStudentGrade = 0;
+
+    // initialize/reset a counter for the number of assignment 
+    int gradedAssignments = 0;
+
+    // loop through the scores array and complete calculations for currentStudent
+    foreach (int score in studentScores)
+    {
+        // increment the assignment counter
+        gradedAssignments += 1;
+
+        if (gradedAssignments <= examAssignments)
+        {
+            sumAssignmentScores += score;
+            studentAverage = (decimal) sumAssignmentScores/examAssignments;
+        }
+        else
+        {
+            sumAssignmentScores += score / 10;
+            extraCreditSum += score;
+        }
+    }
+
+    extraCreditAverage = (decimal) extraCreditSum/(studentScores.Length-examAssignments);
+    pointsExtraCredit = (decimal) (extraCreditSum/10)/examAssignments;
+
+    currentStudentGrade = (decimal)sumAssignmentScores/ examAssignments;
+
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+
+    else
+        currentStudentLetterGrade = "F";
+
+    Console.WriteLine($"{currentStudent}\t\t{studentAverage}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}\t\t{extraCreditAverage} ({pointsExtraCredit})");
+}
+
+Console.WriteLine("\n\rPress the Enter key to continue");
+Console.ReadLine();
